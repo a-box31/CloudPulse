f58aaae70dd68b94850923db75228cabf2690b59
a-box <thomaa2031@gmail.com>
2026-04-01 19:38:22 -0400

    fix: cors issue, needed X-Upload token header

--- server/src/middleware/cors.ts
+++ server/src/middleware/cors.ts
@@ -4,6 +4,6 @@ import { config } from "../config.js";
 export const corsMiddleware = cors({
   origin: config.cloudUrl || "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "X-Server-Key", "X-Server-Id", "Range"],
+  allowedHeaders: ["Content-Type", "X-Server-Key", "X-Server-Id", "X-Upload-Token", "Range"],
   exposedHeaders: ["Content-Range", "Accept-Ranges", "Content-Length"],
 });
